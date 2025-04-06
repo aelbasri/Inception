d81ce6139f0f303f5111e91d9a0b7a8c8b23f571
aelbasri <ayoubelbasri313@gmail.com>
2025-04-06 19:07:56 +0100

    mariadb successfully set up

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,31 +1,12 @@
 #! /bin/bash
 
-#mysql_secure_installation
-
-#echo "[mysqld]" >> /etc/mysql/my.cnf
-#echo "datadir = /var/lib/mysql" >> /etc/mysql/my.cnf
-#echo "socket  = /run/mysqld/mysqld.sock" >> /etc/mysql/my.cnf
-#echo "bind_address=*" >> /etc/mysql/my.cnf
-#echo "port = 3306" >> /etc/mysql/my.cnf
-#echo "user = mysql" >> /etc/mysql/my.cnf
-
-#echo "[mysqld]" >> /etc/mysql/my.cnf
-#echo "socket = /var/run/mysqld/mysqld.sock" >> /etc/mysql/my.cnf
-#echo "[client]" >> /etc/mysql/my.cnf
-#echo "socket = /var/run/mysqld/mysqld.sock" >> /etc/mysql/my.cnf
-#service mysqld start
-
-(echo "CREATE DATABASE $DBNAME;" && \
- echo "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASS';" && \
- echo "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'localhost';" && \
- echo "FLUSH PRIVILEGES;") | mariadb
-
-#service mariadb stop
-
 mariadbd-safe
 
-#echo CREATE USER "$DBUSER"@'localhost' IDENTIFIED BY "$DBPASS";
-#echo GRANT ALL PRIVILEGES ON wordpress.* TO "$DBUSER"@'localhost'; 
-#echo FLUSH PRIVILEGES;  
-#echo EXIT;
+mariadb -e "CREATE DATABASE $DBNAME;"
+mariadb -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASS';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'localhost';" 
+mariadb -e "FLUSH PRIVILEGES;"
 
+mysqladmin -u root shutdown
+
+mariadbd-safe
